e2d07fab9d3484039f43fea1640f7bcf556e2465
Luis Schubert <schubert.luis@gmail.com>
2025-04-17 10:57:57 -0700

    add build.rs

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Compile C library
+    cc::Build::new()
+        .file("c_src/mylib.c")
+        .flag("-O2") // Optional: optimization
+        .target("aarch64-unknown-linux-gnu") // Match cross-compilation target
+        .compile("mylib"); // Outputs libmylib.a
+
+    // Tell Cargo to link libmylib.a
+    println!("cargo:rustc-link-lib=static=mylib");
+    println!("cargo:rustc-link-search=native={}/c_src", env::var("OUT_DIR").unwrap());
+
+    // Generate bindgen bindings
+    let bindings = bindgen::Builder::default()
+        .header("c_src/mylib.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write bindings to file
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
